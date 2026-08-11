c1e173493e5cb7a56c9d3fc2843882e41bdf80ba
Your Name <you@example.com>
2026-08-11 17:33:49 +0300

    1 37 11: restore & rebuild category translations, fix imports

--- lib/components/common/category_picker_sheet.dart
+++ lib/components/common/category_picker_sheet.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import '../store/finance_store.dart';
-import '../theme/theme.dart';
-import '../utils/category_icons.dart';
-import '../utils/translate_category.dart';
+import '../../store/finance_store.dart';
+import '../../theme/theme.dart';
+import '../../utils/category_icons.dart';
+import '../../utils/translate_category.dart';
 
 class CategoryPickerSheet {
   static Future<String?> show(
