e1c77bd6afb7b9c74640707b1e37784816193442
Your Name <you@example.com>
2026-09-14 18:06:27 +0300

    fix: align currency id map with server DB (CNY=11 not PLN, PLN=169), fixes CNY rate showing 0.0 (v1.41.115+274)

--- lib/utils/currency_utils.dart
+++ lib/utils/currency_utils.dart
@@ -1,13 +1,29 @@
 const Map<String, String> currencyIdToCode = {
-  '1': 'RUB', '2': 'USD', '3': 'EUR', '4': 'GBP', '5': 'CHF',
-  '6': 'CNY', '7': 'JPY', '8': 'BYN', '9': 'UAH', '10': 'KZT',
-  '11': 'PLN', '12': 'CZK', '13': 'SEK', '14': 'NOK',
+  '1': 'RUB', '2': 'USD', '3': 'EUR', '4': 'UAH', '5': 'AUD',
+  '6': 'BYR', '7': 'DKK', '8': 'ISK', '9': 'KZT', '10': 'CAD',
+  '11': 'CNY', '12': 'NOK', '13': 'XDR', '14': 'SGD', '15': 'TRY',
+  '16': 'GBP', '17': 'SEK', '18': 'CHF', '19': 'JPY',
+  '20': 'AFN', '21': 'ALL', '23': 'ADP', '24': 'AZN', '30': 'AMD',
+  '41': 'BIF', '53': 'CZK', '54': 'DOP', '59': 'FKP', '70': 'HKD',
+  '71': 'HUF', '72': 'INR', '76': 'ILS', '81': 'KRW', '94': 'MYR',
+  '99': 'MXN', '100': 'MNT', '110': 'NZD', '118': 'PHP', '130': 'VND',
+  '139': 'THB', '158': 'TWD', '163': 'TJS', '169': 'PLN', '170': 'BRL',
+  '174': 'ARS', '176': 'XAU', '177': 'XAG', '178': 'XPT', '184': 'XPD',
+  '186': 'BYN',
 };
 
 const Map<String, String> currencyCodeToId = {
-  'RUB': '1', 'USD': '2', 'EUR': '3', 'GBP': '4', 'CHF': '5',
-  'CNY': '6', 'JPY': '7', 'BYN': '8', 'UAH': '9', 'KZT': '10',
-  'PLN': '11', 'CZK': '12', 'SEK': '13', 'NOK': '14',
+  'RUB': '1', 'USD': '2', 'EUR': '3', 'UAH': '4', 'AUD': '5',
+  'BYR': '6', 'DKK': '7', 'ISK': '8', 'KZT': '9', 'CAD': '10',
+  'CNY': '11', 'NOK': '12', 'XDR': '13', 'SGD': '14', 'TRY': '15',
+  'GBP': '16', 'SEK': '17', 'CHF': '18', 'JPY': '19',
+  'AFN': '20', 'ALL': '21', 'ADP': '23', 'AZN': '24', 'AMD': '30',
+  'BIF': '41', 'CZK': '53', 'DOP': '54', 'FKP': '59', 'HKD': '70',
+  'HUF': '71', 'INR': '72', 'ILS': '76', 'KRW': '81', 'MYR': '94',
+  'MXN': '99', 'MNT': '100', 'NZD': '110', 'PHP': '118', 'VND': '130',
+  'THB': '139', 'TWD': '158', 'TJS': '163', 'PLN': '169', 'BRL': '170',
+  'ARS': '174', 'XAU': '176', 'XAG': '177', 'XPT': '178', 'XPD': '184',
+  'BYN': '186',
 };
 
 const Map<String, String> currencySymbols = {
